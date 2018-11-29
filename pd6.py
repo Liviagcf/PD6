604eb099bf9fbb8e88db77328956c069c7ec0b1b
Natalia <natioliveira97@hotmail.com>
2018-11-29 20:49:46 -0200

    porcentagem de acerto

--- pd6.py
+++ pd6.py
@@ -35,15 +35,34 @@ cap = cv2.VideoCapture(videoname)
 
 param1 = 0.1
 param2 = 0.1
-
 valid = 0
 
-while(True):
+nframes = 0
+nframes_with_ball = 0
+acertos = 0
+
+filename = sys.argv[1]+'_gab.txt'
+
+file = open(filename,"r")
+
+centers = []
+
+for line in file:
+	data = line.split()
+	x=data[0]
+	y=data[1]
+	center = [(x,y)]
+	centers.append(center)
+
+while(nframes<(len(centers)+1)):
     ret, frame = cap.read()
 
     if ret==False:
     	break
-    
+
+    if int(centers[nframes][0][0])>0:
+    	nframes_with_ball = nframes_with_ball+1
+
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     ball = ball_cascade.detectMultiScale(gray, 1.3, 5)
 
@@ -53,12 +72,19 @@ while(True):
     for (x,y,w,h) in ball:
     	valid =  pixelCountCheck(0.1, 0.1, whiteMask, blackMask, (x,y),(x+w,y+h))
     	if valid:
-    		cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+    		x2=x+w
+    		y2=y+h
+	    	if int(centers[nframes][0][0])>=x: #and int(centers[nframes][0][0])<=x2 and int(centers[nframes][0][1])>=y and int(centers[nframes][0][1])<=y2:
+	    		acertos=acertos+1
+	    	cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+    nframes = nframes+1
 
 
     cv2.imshow('ball_detection',frame)
-
     cv2.waitKey(0)
-    
+
+taxa_acertos = (float(acertos)/nframes_with_ball)*100
+print("Taxa de acerto = %s %%" % taxa_acertos)
 
 cv2.destroyAllWindows()
+file.close()
